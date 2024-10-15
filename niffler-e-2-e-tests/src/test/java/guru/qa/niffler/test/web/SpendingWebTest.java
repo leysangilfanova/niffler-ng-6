47f3ca765abbb43ca3272c9f4872cb95f5c0cd53
Leysan <l.gilfanova.kazanexpress@gmail.com>
2024-10-15 23:42:54 +0700

    Домашнее задание 6.3. 4-5 задания

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
@@ -1,9 +1,9 @@
 package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
-import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.model.SpendJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.MainPage;
@@ -34,5 +34,12 @@ public class SpendingWebTest {
                 .save();
 
         new MainPage().checkThatTableContainsSpending(newDescription);
+    }
+
+    @Test
+    void spendCheck() {
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .login("kisa", "kisa")
+                .checkThatTableContainsSpending("Еда");
     }
 }
