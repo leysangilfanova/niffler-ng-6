da90e22a39726604b065cb333b352aaa1de190ed
Leysan <l.gilfanova.kazanexpress@gmail.com>
2024-10-16 00:10:45 +0700

    Revert "Домашнее задание 6.3. 1-3 задания"
    This reverts commit 1242883bc37b96d08dffd021237b11e72b424cf1.

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApiClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApiClient.java
@@ -4,42 +4,36 @@ import guru.qa.niffler.config.Config;
 import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
-import guru.qa.niffler.service.SpendClient;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.List;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SpendApiClient implements SpendClient {
+public class SpendApiClient {
 
-    private final SpendApi spendApi;
+    private final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(Config.getInstance().spendUrl())
+            .addConverterFactory(JacksonConverterFactory.create())
+            .build();
 
-    public SpendApiClient() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Config.getInstance().spendUrl())
-                .addConverterFactory(JacksonConverterFactory.create())
-                .build();
+    private final SpendApi spendApi = retrofit.create(SpendApi.class);
 
-        this.spendApi = retrofit.create(SpendApi.class);
-    }
-
-    @Override
     public SpendJson createSpend(SpendJson spend) {
+        final Response<SpendJson> response;
         try {
-            Response<SpendJson> response = spendApi.addSpend(spend).execute();
-            if (response.isSuccessful()) {
-                return response.body();
-            } else {
-                throw new RuntimeException("Failed to create spend: " + response.errorBody().string());
-            }
+            response = spendApi
+                    .addSpend(spend)
+                    .execute();
         } catch (IOException e) {
-            throw new RuntimeException("Error while creating spend", e);
+            throw new AssertionError(e);
         }
+        assertEquals(201, response.code());
+        return response.body();
     }
 
     public SpendJson editSpend(SpendJson spend) {
@@ -93,18 +87,17 @@ public class SpendApiClient implements SpendClient {
         assertEquals(202, response.code());
     }
 
-    @Override
-    public CategoryJson createCategory(CategoryJson category) {
+    public CategoryJson addCategory(CategoryJson category) {
+        final Response<CategoryJson> response;
         try {
-            Response<CategoryJson> response = spendApi.addCategory(category).execute();
-            if (response.isSuccessful()) {
-                return response.body();
-            } else {
-                throw new RuntimeException("Failed to create category: " + response.errorBody().string());
-            }
+            response = spendApi
+                    .addCategory(category)
+                    .execute();
         } catch (IOException e) {
-            throw new RuntimeException("Error while creating category", e);
+            throw new AssertionError(e);
         }
+        assertEquals(200, response.code());
+        return response.body();
     }
 
     public CategoryJson updateCategory(CategoryJson category) {
@@ -132,10 +125,5 @@ public class SpendApiClient implements SpendClient {
         }
         assertEquals(200, response.code());
         return response.body();
-    }
-
-    @Override
-    public void removeCategory(CategoryJson category) {
-        throw new UnsupportedOperationException("Deleting a category is not supported by API");
     }
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/SpendingExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/SpendingExtension.java
@@ -1,11 +1,12 @@
 package guru.qa.niffler.jupiter.extension;
 
-import guru.qa.niffler.api.SpendApiClient;
 import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.SpendJson;
 import guru.qa.niffler.model.UserJson;
+import guru.qa.niffler.service.SpendClient;
+import guru.qa.niffler.service.SpendDbClient;
 import org.apache.commons.lang3.ArrayUtils;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -22,10 +23,10 @@ public class SpendingExtension implements BeforeEachCallback, ParameterResolver 
 
     public static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(SpendingExtension.class);
 
-    private final SpendApiClient spendApiClient = new SpendApiClient();
+    private final SpendClient spendClient = new SpendDbClient();
 
     @Override
-    public void beforeEach(ExtensionContext context) {
+    public void beforeEach(ExtensionContext context) throws Exception {
         AnnotationSupport.findAnnotation(context.getRequiredTestMethod(), User.class)
                 .ifPresent(userAnno -> {
                     if (ArrayUtils.isNotEmpty(userAnno.spendings())) {
@@ -49,9 +50,10 @@ public class SpendingExtension implements BeforeEachCallback, ParameterResolver 
                                     user != null ? user.username() : userAnno.username()
                             );
 
-                            SpendJson createdSpend = spendApiClient.createSpend(spend);
+                            SpendJson createdSpend = spendClient.createSpend(spend);
                             result.add(createdSpend);
                         }
+
 
                         if (user != null) {
                             user.testData().spendings().addAll(result);
